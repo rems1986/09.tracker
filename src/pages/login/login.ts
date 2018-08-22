c210f92add94391aee19a7a4da828f3f89ca772c
rems1986 <ronysanz19862@gmail.com>
2018-08-22 09:46:17 -0600

    Initial commit

---
+++ src/pages/login/login.ts
@@ -0,0 +1,73 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, AlertController, LoadingController } from 'ionic-angular';
+
+import { ViewChild } from '@angular/core';
+import { Slides } from 'ionic-angular';
+
+
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+
+  @ViewChild(Slides) slides: Slides;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public loadingCtrl:LoadingController) {
+  }
+
+  ionViewDidLoad() {
+    this.slides.paginationType='progress';
+    this.slides.lockSwipes(true);
+    this.slides.freeMode=false;
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+  mostrarAlert()
+  {
+      let alert = this.alertCtrl.create({
+      title: 'Login',
+      inputs: [
+        {
+          name: 'username',
+          placeholder: 'Username'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Login',
+          handler: data => {
+            console.log(data);
+            this.verificarUsuario(data.username);
+          }
+
+        }
+      ]
+    });
+    alert.present();
+
+  }
+
+  verificarUsuario(clave:string)
+  {
+    console.log(clave);
+    let loading = this.loadingCtrl.create({
+      content:'verificando'
+    });
+    loading.present();
+    setTimeout(()=>{
+      loading.dismiss();
+    },3000);
+  }
+
+
+
+}
